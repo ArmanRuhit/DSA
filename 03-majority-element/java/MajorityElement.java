dd6e260a0b5afe3409efbbb740382c33f0c5d8c5
Ruhit Arman <47109532+ArmanRuhit@users.noreply.github.com>
2025-08-11 11:14:17 +0600

    majority element hashmap

---
+++ 03-majority-element/java/MajorityElement.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = { 3, 2, 3 };
+        System.out.println("HashMap: "+majorityElementHashMap(nums));
+    }
+    
+    static int majorityElementHashMap(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            if (countMap.containsKey(num)) {
+                countMap.put(num, countMap.get(num) + 1);
+            } else {
+                countMap.put(num, 1);
+            }
+        }
+        
+        for (int key : countMap.keySet()) {
+            if (countMap.get(key) > nums.length / 2) {
+                return key;
+            }
+        }
+        
+        return 0;
+    }
+}
